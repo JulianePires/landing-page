77d7b9e3d21c44b0be0cb4b205cfe6257419f369
Juliane Pires <mpiresjuliane@outlook.com>
2020-06-29 16:57:35 -0300

    adição da página desktop e atualização do index

--- js/script.js
+++ js/script.js
@@ -47,3 +47,15 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "Oferta Expirada!";
   }
 }, 1000);
+
+function fecharModal()
+{
+  document.getElementById('fundo').style.display = 'none';
+  document.getElementById('modal').style.display = 'none';
+}
+
+function abrirModal()
+{
+  document.getElementById('fundo').style.display = 'block';
+  document.getElementById('modal').style.display = 'block';
+}
